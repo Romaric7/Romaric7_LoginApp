3f3992245bcc287189a4b34ac2c3d4c08137d5ee
Romaric7 <64481167+Romaric7@users.noreply.github.com>
2020-05-13 19:17:08 +0100

    Premier commit de depot des codes

---
+++ create_account.php
@@ -0,0 +1,46 @@
+<?php
+$servername = "localhost";
+$serverport = 5432;
+$serverusername = "postgres";
+$password = "admin";
+$dbname = "HerokuLogin";
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$account_password = $_POST['password'];
+$phone_number = $_POST['number'];
+
+$newpassword = md5($account_password);
+
+
+// Create connection
+$conn = pg_connect("host=".$servername." dbname=".$dbname." user=".$serverusername." password=".$password);
+
+if (!$conn) {
+    die("Connection failed: " . pg_connect_error());
+}
+
+$sql = "INSERT INTO account (username, email, account_password,phone_number)
+VALUES ('$username', '$email', '$newpassword', '$phone_number')";
+
+
+if (pg_query($conn, $sql)) {
+    echo "<script>alert('SUCCES DE CREATION DE COMPTE.')</script>";
+} else {
+    echo "Error: " . $sql . "<br>" . pg_error($conn);
+}
+
+pg_close($conn);
+
+?> 
+
+       <!--redirection vers page de création de compte -->
+        <script>
+
+            window.open("index_account.html","_self");
+
+        </script>
+        
+
+
+
---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "localhost";
+$serverport = 5432;
+$serverusername = "postgres";
+$password = "admin";
+$dbname = "HerokuLogin";
+
+$username = $_POST['username'];
+$account_password = $_POST['password'];
+
+$newpassword = md5($account_password);
+
+
+
+// Create connection
+$conn = pg_connect("host=".$servername." dbname=".$dbname." user=".$serverusername." password=".$password);
+
+//echo "host=".$servername." dbname=".$dbname." user=".$username." password=".$password;
+
+if (!$conn) {
+    die("Connection failed: " . pg_connect_error());
+}
+
+$sql = "SELECT * FROM account WHERE username = '$username' AND account_password= '$newpassword'";
+$result = pg_query($conn, $sql);
+
+if (pg_num_rows($result) > 0) {
+    //selection des data de chaque ligne/tuple/enregistrement 
+    while($row = pg_fetch_assoc($result)) {
+        echo "<script>alert('Connexion effectuée avec succès')</script>";
+    }
+} else  { ?>
+
+    <script>alert("Echec de connexion, nom d'utilisateur ou mot de passe incorrect")</script>
+
+    <?php
+}
+
+
+pg_close($conn);
+
+?>
+
+ <!--redirection vers page de login -->
+ <script>
+
+window.open("index.html","_self");
+
+</script>
